053044cdf922dada1b8b6937744d2256e688f45d
piyush0189 <piyushpushkar2004@gmail.com>
2025-02-05 19:19:10 +0530

    logic added

--- FloydTriangle.java
+++ FloydTriangle.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
 public class FloydTriangle {
+    public static void Floyd_Triangle(int n){
+        //outer
+        int counter = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
-        
+        Floyd_Triangle(5);
     }
 }
